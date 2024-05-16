6575255de070639d384a4c346156022e3231a110
LisiyLexa <dudnik.a.v@mail.com>
2024-05-16 23:30:08 +0500

    website init

---
+++ static/js/main.js
@@ -0,0 +1,48 @@
+function sendRequest(route, resultHandler) {
+  // Get the data to send to the server
+  var data = {
+    user_prompt: document.getElementById("prompt").value,
+  };
+
+  // Send a POST request to Flask route
+  fetch(route, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      // Handle the result
+      console.log(result);
+      if ("error" in result) {
+        alert(result.error);
+        return;
+      }
+      resultHandler(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+function setInnerHTML(selectors, values) {
+  // Setting each selector it's value in values array
+  selectors.forEach((selector, index) => {
+    document.querySelector(selector).innerHTML = values[index];
+  });
+}
+
+document.getElementById("generate").addEventListener("click", function () {
+  sendRequest("/api/get_image", function (result) {
+    let response = result;
+    setInnerHTML(
+      ["#user-prompt", "#optimized-prompt"],
+      [response.user_prompt, response.optimized_prompt]
+    );
+    document.querySelector("#user-image img").setAttribute("src", response.user_image);
+    document.querySelector("#optimized-image img").setAttribute("src", response.optimized_image);
+    document.getElementById("images").classList.remove("hidden");
+  });
+});
