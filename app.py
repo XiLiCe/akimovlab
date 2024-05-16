2187516c3570bef9a878f744c6a77d16954b6c21
LisiyLexa <dudnik.a.v@mail.com>
2024-05-16 23:29:49 +0500

    commented

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import render_template, url_for, request, jsonify
+from flask import Flask
+
 from peft import AutoPeftModelForCausalLM
 from transformers import AutoTokenizer
 import requests
+
 import torch
 import requests
 import json
@@ -9,48 +12,48 @@ import base64
 
 app = Flask(__name__)
 
-# Указываем путь к вашей натренированной модели и токенизатору
-MODEL_PATH = "modellamma"
-TOKENIZER_PATH = "modellamma"
+MODEL_PATH = "modellamma"  # Model path
+TOKENIZER_PATH = "modellamma"  # Tokenizer path
+SD_API_ENDPOINT = "http://127.0.0.1:7860/sdapi/v1/txt2img"  # stablediffusion api url
 
-# Загружаем модель и токенизатор
-model = AutoPeftModelForCausalLM.from_pretrained(
+
+# load model and tokenizer
+MODEL = AutoPeftModelForCausalLM.from_pretrained(
     MODEL_PATH,
     load_in_4bit=True,
 )
-tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
+TOKENIZER = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
 
-# Функция для генерации текста с помощью модели
-def generate_text(prompt):
-    model_input = tokenizer(prompt, return_tensors="pt").to("cuda")
-    model.eval()
-    with torch.no_grad():
-        outputs = model.generate(**model_input, max_length=128, repetition_penalty=1.15)
-    return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-# Функция для отправки промпта в API стабильной диффузии и получения картинки
-def get_image(prompt):
-    # api_url = "http://127.0.0.1:7860"  # Замените на URL вашего API стабильной диффузии
-    # response = requests.post(url=f'{api_url}/sdapi/v1/txt2img', json={"prompt": prompt})
-    # return response.content
+def generate_text(prompt: str) -> str:
+    """Optimize user's prompt
+    :return: advanced prompt
+    """
+    inputs = TOKENIZER([f"<human>: {prompt}\n<bot>: "], return_tensors="pt").to("cuda")
 
-    url = "http://127.0.0.1:7860"
+    outputs = MODEL.generate(
+        **inputs, max_new_tokens=64, use_cache=True, repetition_penalty=1.15
+    )
+    return TOKENIZER.batch_decode(outputs, skip_special_tokens=True)
 
-    payload = {
-        "prompt": prompt,
-        "steps": 5
-    }
+
+def get_image(prompt: str) -> str:
+    """Generates image by given prompt\n
+    `prompt` - text description of image\n
+    `return` - string representation of base64-encoded image
+    """
+
+    payload = {"prompt": prompt, "steps": 5}
 
     # Send said payload to said URL through the API.
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    response = requests.post(url=SD_API_ENDPOINT, json=payload)
     r = response.json()
-    image_data = base64.b64decode(r['images'][0])
-    with open("output.png", 'wb') as f:
+    image_data = base64.b64decode(r["images"][0])
+    with open("output.png", "wb") as f:
         f.write(image_data)
 
-    # Возвращаем изображение в формате base64
-    return base64.b64encode(image_data).decode('utf-8')
-
+    # encoded image
+    return base64.b64encode(image_data).decode("utf-8")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -59,9 +62,11 @@ def index():
         prompt = request.form["prompt"]
         generated_text = generate_text(prompt)
         image = get_image(generated_text)
-        return render_template("index.html", prompt=prompt, generated_text=generated_text, image=image)
+        return render_template(
+            "index.html", prompt=prompt, generated_text=generated_text, image=image
+        )
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(port=8080)
-
--- model.py
+++ model.py
@@ -11,16 +11,11 @@ model = AutoPeftModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
+inputs = tokenizer([r"<human>: allien building pc\n<bot>: "], return_tensors="pt").to(
+    "cuda"
+)
 
-model_input = tokenizer(r"<human>: allien building pc\n<bot>: ", return_tensors="pt").to("cuda")
-model.eval()
-with torch.no_grad():
-    print(tokenizer.decode(model.generate(**model_input, max_new_tokens=60, repetition_penalty=1.15)[0], skip_special_tokens=True))
-
-inputs = tokenizer(
-[
-    r"<human>: allien building pc\n<bot>: "
-], return_tensors = "pt").to("cuda")
-
-outputs = model.generate(**inputs, max_new_tokens = 64, use_cache = True)
-tokenizer.batch_decode(outputs)
+outputs = model.generate(
+    **inputs, max_new_tokens=64, use_cache=True, repetition_penalty=1.15
+)
+tokenizer.batch_decode(outputs, skip_special_tokens=True)
